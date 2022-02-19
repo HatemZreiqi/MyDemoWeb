1e2f0787d22ac17f5ada68f18f80a13dc1534eb4
Hatem Zreiqi <zreeqy.hatem0@gmail.com>
2022-02-19 23:16:26 +0200

    v.1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hatem is the Best !!");
 		System.out.println("Hatem is the Best 22 !!");
+		System.out.println("Hatem is the Best 22 !!");
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
